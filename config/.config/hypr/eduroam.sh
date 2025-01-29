68775dca3c8b6c28feb092437d138c3180edb140
Yrjar Vederhus <yrjarvederhus@gmail.com>
2025-01-29 15:42:25 +0100

    fikse eduroam-script for å ikke ødelegge hjemme-pc

--- config/.config/hypr/eduroam.sh
+++ config/.config/hypr/eduroam.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 
+if ! iwctl station wlan0 get-networks | grep -q eduroam; then
+    exit 1
+fi
+
 sudo systemctl stop NetworkManager
 sudo systemctl stop wpa_supplicant
 sudo wpa_supplicant -B -i wlan0 -c ~/.config/cat_installer/cat_installer.conf
